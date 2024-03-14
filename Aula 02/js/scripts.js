7552d219646c38df84fc978d4e03ba8cee7cd4c3
GugaAlencar <129134056+GugaAlencar@users.noreply.github.com>
2024-03-14 20:35:38 -0300

    Atualização
    Código atualizado

---
+++ Aula 02/js/scripts.js
@@ -0,0 +1,10 @@
+function mostra(){
+    document.getElementById('sobre').style.display='block'
+    document.getElementById('sobre').style.position='relative'
+    document.getElementById('sobre').style.top='40px'
+}
+
+function fechar(){
+    document.getElementById('sobre').style.display='none'
+
+}
